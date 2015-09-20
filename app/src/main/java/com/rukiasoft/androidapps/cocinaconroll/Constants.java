1aa9a620fb2379f58e869f91608c49deee7dba19
Raúl Feliz <raulfeliz@gmail.com>
2015-09-21 00:58:25 +0200

    notif??

---
+++ app/src/main/java/com/rukiasoft/androidapps/cocinaconroll/Constants.java
@@ -0,0 +1,8 @@
+package com.rukiasoft.androidapps.cocinaconroll;
+
+/**
+ * Created by iRuler on 18/9/15.
+ */
+public class Constants {
+    public static String SENDER_ID = "962580870211";
+}
--- app/src/main/java/com/rukiasoft/androidapps/cocinaconroll/RecipeListActivity.java
+++ app/src/main/java/com/rukiasoft/androidapps/cocinaconroll/RecipeListActivity.java
@@ -1,16 +1,36 @@
 package com.rukiasoft.androidapps.cocinaconroll;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.rukiasoft.androidapps.cocinaconroll.gcm.GcmRegistrationAsyncTask;
+import com.rukiasoft.androidapps.cocinaconroll.gcm.RegistrationIntentService;
+
 public class RecipeListActivity extends AppCompatActivity {
+
+    private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+    private static final String TAG = "MainActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_list);
+
+        if (checkPlayServices()) {
+
+            // Start IntentService to register this application with GCM.
+            Intent intent = new Intent(this, RegistrationIntentService.class);
+            //startService(intent);
+            new GcmRegistrationAsyncTask(this).execute();
+        }
+
     }
 
 
@@ -35,4 +55,29 @@ public class RecipeListActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+    /**
+     * Check the device to make sure it has the Google Play Services APK. If
+     * it doesn't, display a dialog that allows users to download the APK from
+     * the Google Play Store or enable it in the device's system settings.
+     */
+    private boolean checkPlayServices() {
+        GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
+        int resultCode = apiAvailability.isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (apiAvailability.isUserResolvableError(resultCode)) {
+                apiAvailability.getErrorDialog(this, resultCode, PLAY_SERVICES_RESOLUTION_REQUEST)
+                        .show();
+            } else {
+                Log.i(TAG, "This device is not supported.");
+                finish();
+            }
+            return false;
+        }
+        return true;
+    }
+
+
+
 }
