a70f5442929642af465e87673e693676d0e7cf63
kunotatemaki <raulfeliz@gmail.com>
2015-11-16 15:09:52 +0100

    delete comments

--- app/src/main/java/com/rukiasoft/androidapps/cocinaconroll/ui/ShowSigningActivity.java
+++ app/src/main/java/com/rukiasoft/androidapps/cocinaconroll/ui/ShowSigningActivity.java
@@ -15,23 +15,17 @@
  */
 package com.rukiasoft.androidapps.cocinaconroll.ui;
 
-import android.app.ProgressDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.IntentSender;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.OptionalPendingResult;
 import com.google.android.gms.common.api.ResultCallback;
@@ -39,10 +33,7 @@ import com.google.android.gms.common.api.Status;
 import com.rukiasoft.androidapps.cocinaconroll.CocinaConRollApplication;
 import com.rukiasoft.androidapps.cocinaconroll.R;
 import com.rukiasoft.androidapps.cocinaconroll.utilities.Constants;
-import com.rukiasoft.androidapps.cocinaconroll.utilities.ReadWriteTools;
 import com.rukiasoft.androidapps.cocinaconroll.utilities.Tools;
-
-import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -60,9 +51,7 @@ public class ShowSigningActivity extends SigningDriveActivity {
     @Bind(R.id.sign_in_button)SignInButton signInButton;
     @Bind(R.id.sign_out_button)Button signOutButton;
     @Bind(R.id.sign_in_discard_button)Button discardButton;
-    @Bind(R.id.sign_in_icon)ImageView signInIcon;
 
-    private ProgressDialog mProgressDialog;
     private String accountName;
 
 
@@ -116,34 +105,6 @@ public class ShowSigningActivity extends SigningDriveActivity {
     }
 
     private void updateUI(boolean isSignedIn) {
-        /*if (isSignedIn) {
-            // Show signed-in user's name
-            Person currentPerson = Plus.PeopleApi.getCurrentPerson(getMyApplication().getGoogleApiClient());
-            if (currentPerson != null) {
-                String name = currentPerson.getDisplayName();
-                mStatus.setText(getString(R.string.signed_in_fmt, name));
-                Tools mTools = new Tools();
-                mTools.savePreferences(this, Constants.PROPERTY_CLOUD_BACKUP, true);
-                ReadWriteTools rwTools = new ReadWriteTools(this);
-                rwTools.loadImageFromPathInCircle(signInIcon, currentPerson.getImage().getUrl(), R.mipmap.ic_launcher);
-
-            } else {
-                Log.w(TAG, getString(R.string.error_null_person));
-                mStatus.setText(getString(R.string.signed_in_err));
-            }
-
-            // Set button visibility
-            signInButton.setVisibility(View.GONE);
-            signOutButton.setVisibility(View.VISIBLE);
-            discardButton.setText(getString(R.string.exit));
-        } else {
-            // Show signed-out message
-            mStatus.setText(R.string.signed_out);
-
-            // Set button visibility
-            signInButton.setEnabled(true);
-            signInButton.setVisibility(View.VISIBLE);
-        }*/
 
         if (isSignedIn) {
             mStatus.setText(accountName);
@@ -178,14 +139,7 @@ public class ShowSigningActivity extends SigningDriveActivity {
             // this asynchronous branch will attempt to sign in the user silently.  Cross-device
             // single sign-on will occur in this branch.
             updateUI(false);
-            /*showProgressDialog();
-            opr.setResultCallback(new ResultCallback<GoogleSignInResult>() {
-                @Override
-                public void onResult(GoogleSignInResult googleSignInResult) {
-                    hideProgressDialog();
-                    handleSignInResult(googleSignInResult);
-                }
-            });*/
+
         }
     }
 
@@ -218,21 +172,7 @@ public class ShowSigningActivity extends SigningDriveActivity {
     }
     // [END onActivityResult]
 
-    private void showProgressDialog() {
-        if (mProgressDialog == null) {
-            mProgressDialog = new ProgressDialog(this);
-            mProgressDialog.setMessage(getString(R.string.loading));
-            mProgressDialog.setIndeterminate(true);
-        }
 
-        mProgressDialog.show();
-    }
-
-    private void hideProgressDialog() {
-        if (mProgressDialog != null && mProgressDialog.isShowing()) {
-            mProgressDialog.hide();
-        }
-    }
 
     // [START signIn]
     private void signIn() {
@@ -242,7 +182,7 @@ public class ShowSigningActivity extends SigningDriveActivity {
     // [END signIn]
 
     // [START signOut]
-    private void signOut() {
+    /*private void signOut() {
         Auth.GoogleSignInApi.signOut(getMyApplication().getGoogleApiClient()).setResultCallback(
                 new ResultCallback<Status>() {
                     @Override
@@ -252,7 +192,7 @@ public class ShowSigningActivity extends SigningDriveActivity {
                         // [END_EXCLUDE]
                     }
                 });
-    }
+    }*/
     // [END signOut]
 
     // [START revokeAccess]
@@ -273,24 +213,6 @@ public class ShowSigningActivity extends SigningDriveActivity {
     }
     // [END revokeAccess]
 
-    /*@Override
-    public void onConnected(Bundle bundle) {
-        // onConnected indicates that an account was selected on the device, that the selected
-        // account has granted any requested permissions to our app and that we were able to
-        // establish a service connection to Google Play services.
-        Log.d(TAG, "onConnected:" + bundle);
-
-        // Show the signed-in UI
-        updateUI(true);
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-        // The connection to Google Play services was lost. The GoogleApiClient will automatically
-        // attempt to re-connect. Any UI elements that depend on connection to Google APIs should
-        // be hidden or disabled until onConnected is called again.
-        Log.w(TAG, "onConnectionSuspended:" + i);
-    }*/
 
     // [START on_connection_failed]
     @Override
@@ -300,51 +222,9 @@ public class ShowSigningActivity extends SigningDriveActivity {
         // ConnectionResult to see possible error codes.
         Log.d(TAG, "onConnectionFailed:" + connectionResult);
 
-        /*if (!mIsResolving && mShouldResolve) {
-            if (connectionResult.hasResolution()) {
-                try {
-                    connectionResult.startResolutionForResult(this, RC_SIGN_IN);
-                    mIsResolving = true;
-                } catch (IntentSender.SendIntentException e) {
-                    Log.e(TAG, "Could not resolve ConnectionResult.", e);
-                    mIsResolving = false;
-                    getMyApplication().getGoogleApiClient().connect();
-                }
-            } else {
-                // Could not resolve the connection result, show the user an
-                // error dialog.
-                showErrorDialog(connectionResult);
-            }
-        } else {
-            // Show the signed-out UI
-            updateUI(false);
-        }*/
+
     }
     // [END on_connection_failed]
-
-    private void showErrorDialog(ConnectionResult connectionResult) {
-        int errorCode = connectionResult.getErrorCode();
-
-        if (GooglePlayServicesUtil.isUserRecoverableError(errorCode)) {
-            // Show the default Google Play services error dialog which may still start an intent
-            // on our behalf if the user can resolve the issue.
-            GooglePlayServicesUtil.getErrorDialog(errorCode, this, RC_SIGN_IN,
-                    new DialogInterface.OnCancelListener() {
-                        @Override
-                        public void onCancel(DialogInterface dialog) {
-                            mShouldResolve = false;
-                            updateUI(false);
-                        }
-                    }).show();
-        } else {
-            // No default Google Play Services error, display a message to the user.
-            String errorString = getString(R.string.play_services_error_fmt, errorCode);
-            Toast.makeText(this, errorString, Toast.LENGTH_SHORT).show();
-
-            mShouldResolve = false;
-            updateUI(false);
-        }
-    }
 
 
 }
